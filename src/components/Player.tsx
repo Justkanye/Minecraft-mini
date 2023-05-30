24b3487c521e6d9ef1d6f43a6921cfb3f2137938
Justkanye <adekanyetimilehin2000@gmail.com>
2023-05-30 06:01:52 +0100

    player movement

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,24 +1,58 @@
 import type { Triplet } from '@react-three/cannon';
 import { useSphere } from '@react-three/cannon';
 import { useFrame, useThree } from '@react-three/fiber';
-import type { FC } from 'react';
+import type { FC, MutableRefObject } from 'react';
+import { useEffect, useRef } from 'react';
+import { Vector3 } from 'three';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+
+import { useKeyboardControls } from '@/hooks';
+
+const SPEED = 6;
 
 const Player: FC<Props> = ({ position }) => {
-  const { camera } = useThree();
-  const [ref] = useSphere(() => ({
+  const { camera, gl } = useThree();
+  const [ref, api] = useSphere(() => ({
     type: 'Dynamic',
     mass: 1,
     position,
   }));
+  const velocity: MutableRefObject<Triplet> = useRef([0, 0, 0]);
+  const controls = new PointerLockControls(camera, gl.domElement);
+
+  useEffect(() => {
+    api.velocity.subscribe((v) => {
+      velocity.current = v;
+    });
+    api.position.subscribe((p) => {
+      camera.position.copy(new Vector3(...p));
+    });
+    document.addEventListener('click', () => controls.lock());
+  }, []);
+
+  const { moveBackward, moveForward, moveLeft, moveRight, jump } =
+    useKeyboardControls();
 
   useFrame(() => {
-    if (ref.current) camera.position.copy(ref.current.position);
+    const frontZ = (moveBackward ? 1 : 0) - (moveForward ? 1 : 0);
+    const sideX = (moveLeft ? 1 : 0) - (moveRight ? 1 : 0);
+    const direction = new Vector3();
+    const frontVector = new Vector3(0, 0, frontZ);
+    const sideVector = new Vector3(sideX, 0, 0);
+    const y =
+      jump && parseFloat(Math.abs(velocity.current[1]).toFixed(2)) < 0.05
+        ? 8
+        : velocity.current[1];
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+    api.velocity.set(direction.x, y, direction.z);
   });
 
-  return (
-    // @ts-ignore
-    <mesh castShadow ref={ref} />
-  );
+  // @ts-ignore
+  return <mesh castShadow ref={ref} />;
 };
 
 export default Player;
--- src/hooks/useKeyboardcontrols.ts
+++ src/hooks/useKeyboardcontrols.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+import { CubeTexture } from '@/types';
+
 import { useStore } from './useStore';
 
 export const useKeyboardControls = () => {
@@ -13,11 +15,37 @@ export const useKeyboardControls = () => {
   });
 
   useEffect(() => {
+    const getActionByKey = (key: string): string | null => {
+      const keys = {
+        ArrowUp: 'moveForward',
+        ArrowDown: 'moveBackward',
+        ArrowLeft: 'moveLeft',
+        ArrowRight: 'moveRight',
+        Space: 'jump',
+      };
+      // @ts-ignore
+      if (Object.keys(keys).includes(key)) return keys[key];
+      return null;
+    };
+    const getTextureByKey = (key: string): number | null => {
+      const textures = {
+        Digit1: CubeTexture.WOOD,
+        Digit2: CubeTexture.WATER,
+        Digit3: CubeTexture.GRASS,
+      };
+      // @ts-ignore
+      if (Object.keys(textures).includes(key)) return textures[key];
+      return null;
+    };
     const handleKeyDown = (e: KeyboardEvent) => {
-      console.log({ key: e.key, code: e.code });
+      const action = getActionByKey(e.code);
+      const texture = getTextureByKey(e.code);
+      if (action) setMovement((prev) => ({ ...prev, [action]: true }));
+      if (texture !== null) setTexture(texture);
     };
     const handleKeyUp = (e: KeyboardEvent) => {
-      console.log({ key: e.key, code: e.code });
+      const action = getActionByKey(e.code);
+      if (action) setMovement((prev) => ({ ...prev, [action]: false }));
     };
     document.addEventListener('keydown', handleKeyDown);
     document.addEventListener('keyup', handleKeyUp);
@@ -27,4 +55,6 @@ export const useKeyboardControls = () => {
       document.removeEventListener('keyup', handleKeyUp);
     };
   }, []);
+
+  return movement;
 };
