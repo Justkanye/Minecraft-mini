12f03bff310329a316d1a7c0f4191c081f12e7ef
Justkanye <adekanyetimilehin2000@gmail.com>
2023-05-30 06:02:24 +0100

    show available textures

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,13 +1,13 @@
 import { Physics } from '@react-three/cannon';
 import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
 import { PCFSoftShadowMap } from 'three';
 
 import { useStore } from '../hooks';
 import Ground from './Base';
 import Cube from './Cube';
 import Player from './Player';
+import Textures from './Textures';
 
 const Scene = () => {
   const cubes = useStore((s) => s.cubes);
@@ -20,24 +20,19 @@ const Scene = () => {
       }}
       id="canvas-container"
     >
-      <Canvas
-        // camera={{ position: [4, 2, 5], fov: 75, near: 0.1, far: 100 }}
-        color={'#262837'}
-        shadows={{ type: PCFSoftShadowMap }}
-      >
+      <Canvas color={'#262837'} shadows={{ type: PCFSoftShadowMap }}>
         <Sky sunPosition={[100, 20, 100]} />
         <ambientLight intensity={0.25} />
         <pointLight castShadow intensity={0.7} position={[100, 100, 100]} />
         <Physics gravity={[0, -30, 0]}>
-          <Suspense fallback={null}>
-            <Ground position={[0, 0.5, 0]} />
-            <Player position={[0, 3, 10]} />
-            {cubes.map((cube) => (
-              <Cube key={cube.pos.join('-')} cube={cube} />
-            ))}
-          </Suspense>
+          <Ground position={[0, 0.5, 0]} />
+          <Player position={[0, 5, 10]} />
+          {cubes.map((cube) => (
+            <Cube key={cube.pos.join('-')} {...cube} />
+          ))}
         </Physics>
       </Canvas>
+      <Textures />
     </div>
   );
 };
